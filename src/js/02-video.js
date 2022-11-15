6e00fd37be8af68d50d0127a018a408200b3432b
Vladislav Ulianeev <vladis.ulian@gmail.com>
2022-11-15 21:14:29 +0200

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(element => {
+    localStorage.setItem(
+      'videoplayer-current-time',
+      JSON.stringify(element.seconds)
+    );
+  }, 1000)
+);
+
+if (localStorage.getItem('videoplayer-current-time') !== null) {
+  player
+    .setCurrentTime(Number(localStorage.getItem('videoplayer-current-time')))
+    .catch(function (error) {
+      console.error(error);
+    });
+}
